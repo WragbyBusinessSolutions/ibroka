c4a16d02dcb76883b6097d566c170233bc88d133
Emmanuel Temofe Anene <Eanene@wragbysolutions.com>
2019-10-14 17:05:40 +0100

    Latest

---
+++ ibroka/Controllers/LeadManagement/Claims.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ibroka.Controllers.LeadManagement
+{
+    public class Claims : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}

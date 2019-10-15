f5e9a0e162b5a5ea77c17d3d07704ca8b668da1c
Elijah Adewuyi <Eadewuyi@wragbysolutions.com>
2019-10-15 10:46:33 +0100

    incomeexpese

---
+++ ibroka/Models/AccountBroker/IncomeType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ibroka.Models.AccountBroker
+{
+  public class IncomeType : BaseClass
+  {
+    public Guid Id { get; set; }
+    public string IncomeName { get; set; }
+    public string Description { get; set; }
+
+  }
+}
